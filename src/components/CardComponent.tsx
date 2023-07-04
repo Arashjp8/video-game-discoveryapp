9abbca8cd7a8a88b1495a626c1cbf78204c7f16b
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-04 18:01:56 +0330

    get optimized images

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -9,6 +9,7 @@ import {
 import Game from "../interfaces/Game";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
+import getCroppedImageURL from "../services/image-url";
 
 const CardComponent = ({ game }: { game: Game }) => {
   const cardBackgroundColor = useColorModeValue(
@@ -25,13 +26,8 @@ const CardComponent = ({ game }: { game: Game }) => {
         borderRadius={"20px"}
         overflow={"hidden"}
         cursor={"pointer"}
-        paddingBottom={2}
       >
-        <Image
-          height={"300px"}
-          objectFit={"cover"}
-          src={game.background_image}
-        />
+        <Image src={getCroppedImageURL(game.background_image)} />
         <CardBody>
           <Heading fontSize={"2xl"}>{game.name}</Heading>
           <HStack justifyContent={"space-between"}>
@@ -41,49 +37,6 @@ const CardComponent = ({ game }: { game: Game }) => {
             <CriticScore score={game.metacritic} />
           </HStack>
         </CardBody>
-        {/* <Flex flexDirection={"column"}>
-          <Image
-            height={"300px"}
-            objectFit={"cover"}
-            src={game.background_image}
-            borderRadius={"20px"}
-          />
-          <Flex
-            flexDirection={"row"}
-            justifyContent={"space-between"}
-            alignItems={"center"}
-            margin={2}
-          >
-            <Text>
-              {game.parent_platforms.map((platform) => (
-                <Icon
-                  key={platform.platform.id}
-                  as={iconMap[platform.platform.slug]}
-                  margin={1}
-                  fontSize={"large"}
-                />
-              ))}
-            </Text>
-            <Spacer />
-            <Text
-              border={"1px solid"}
-              borderColor={metacriticColor}
-              color={metacriticColor}
-              paddingLeft={2}
-              paddingRight={2}
-              borderRadius={"10px"}
-              fontSize={"large"}
-            >
-              {" "}
-              {game.metacritic}
-            </Text>
-          </Flex>
-          <Flex marginLeft={2}>
-            <Text fontSize={"3xl"} fontWeight={"semibold"}>
-              {game.name}
-            </Text>
-          </Flex>
-        </Flex> */}
       </Card>
     </>
   );
