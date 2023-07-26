5e8f5a0ea0a19b61f31d2790cf250870ebd38bfe
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-26 14:46:09 +0330

    updating react-query

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
           />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside" paddingX={5}>
+          <GridItem area="aside" paddingX={5} marginTop={10}>
             <SideBar
               selectedGenre={gameQuery.genre}
               onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
