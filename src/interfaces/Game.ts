a6e9d02396ad06763e41c64b9ffdacc716486b48
Arashjp8 <arashjafarpour723@gmail.com>
2023-08-03 15:00:23 +0330

    adding hovereEffect and routing to gameDetails to cards

--- src/interfaces/Game.ts
+++ src/interfaces/Game.ts
@@ -4,6 +4,7 @@ import Platform from "./Platform";
 export default interface Game {
   id: number;
   name: string;
+  slug: string;
   background_image: string;
   released: string;
   parent_platforms: { platform: Platform }[];
