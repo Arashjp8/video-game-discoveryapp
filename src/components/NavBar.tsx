973690cbffaef89487770a775460e18d8aa2c4ee
Arashjp8 <arashjafarpour723@gmail.com>
2023-08-05 11:00:56 +0330

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,18 @@
 import { HStack, Text, useColorModeValue } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchBox from "./SearchBox";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const textColor = useColorModeValue("blackAlpha.800", "whiteAlpha.800");
 
   return (
     <HStack padding={"10px"} justifyContent={"space-between"}>
-      <Text as={"h2"} fontSize={"2xl"} color={textColor}>
-        LOGO
-      </Text>
+      <Link to="/">
+        <Text as={"h2"} fontSize={"2xl"} color={textColor}>
+          LOGO
+        </Text>
+      </Link>
       <SearchBox />
       <ColorModeSwitch />
     </HStack>
